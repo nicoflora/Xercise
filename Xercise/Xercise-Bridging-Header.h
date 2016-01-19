d01c0b15bddebb801832a312b228b28fef062c8b
blazierkyle <blazierkyle@gmail.com>
2016-01-19 12:54:40 -0500

    Updated frameworks

---
+++ Xercise/Xercise-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Xercise-Bridging-Header.h
+//  Xercise
+//
+//  Created by Kyle Blazier on 1/19/16.
+//  Copyright © 2016 Kyle Blazier. All rights reserved.
+//
+
+#ifndef Xercise_Bridging_Header_h
+#define Xercise_Bridging_Header_h
+
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
